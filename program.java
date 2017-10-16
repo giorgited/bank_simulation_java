9dd9bed64492e55dfe0e9902cda1aca3fc7518fa
giorgited <gtediashvili@codeauthority.com>
2017-10-16 14:49:14 -0500

    initial push

---
+++ program.java
@@ -0,0 +1,16 @@
+/*
+        Giorgi Tediashvili
+        Project 2
+        10/16/2017
+ */
+
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.Semaphore;
+
+/**
+ *
+ * @author Giorgi Tediashvili
+ */
+ 
