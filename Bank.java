cb605545a386d395da5a0c450bed26b9bd9693cb
giorgited <gxt150530@utdallas.edu>
2017-10-28 11:23:11 -0500

    further changes

--- Bank.java
+++ Bank.java
@@ -40,9 +40,7 @@ public class Bank {
     public static final int CUSTOMER_TASK_TIME = 400;//4 minutes -> 4/10sec;
     public static final int lOANER_TASK_TIME = 100;//1 minute -> 1/10sec;
     // **** oTHER *** //
-    public static Queue < Integer > loan_queue = new ConcurrentLinkedQueue < > ();
-    public static Queue < Integer > teller_queue = new ConcurrentLinkedQueue < > ();
-    
-    //TODO: add in customers info here (balance, custNum, loanAmount) and use mutex to update on requests
-    
+    public static Queue <Integer> loan_queue = new ConcurrentLinkedQueue <>();
+    public static Queue <Integer> teller_queue = new ConcurrentLinkedQueue <>();
+
 }
